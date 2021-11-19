dd4a135d8fd7c2756a95741569af22b91839d50e
william reid gill <blueyeboyreid@gmail.com>
2021-11-19 13:29:37 -0700

    Weapon Schema

--- Models/Weapon-Schema.js
+++ Models/Weapon-Schema.js
@@ -1,12 +1,60 @@
 const mongoose = require("mongoose")
 
-const JobSchema = new mongoose.Schema({
+
+const WeaponSchema = new mongoose.Schema({
     EquipmentType: {
         type: String,
+        required: [true, 'must choose weapon'],
         enum:{
-            values: ['ikea', 'liddy', 'caressa', 'marcos'],
+            values: ['claymore', 'longsword', 'shortsword', 'rapier', 'saber', 
+                'knife', 'dagger', 'spear', 'halberd', 'trident', 'club', 'mace', 
+                'morning star', 'boomerang', 'throwing star', 'throwing Knife', 
+                'longbow', 'shortbow', 'crossbow', 'whip', 'tomahawk', 'battle axe', 
+                'knuckles', 'ring sword', 'katana', 'swallow', 'scythe', 'staff', 
+                'sickle', 'parasol', 'sai'],
+            message: '{VALUE} is not supported'
+
+        }
+    },
+    Attribute1: {
+        type: String,
+        required: [true, 'must choose a Attribute or select "none"'],
+        enum:{
+            values: [ 'none','fire', 'water', 'wind', 'earth', 'light', 'dark', 'poison', 'ice', 'electricity', 'ichor'],
+            message: '{VALUE} is not supported'
+        },
+        default: "none",
+    },
+    Attribute2: {
+        type: String,
+        required: [true, 'must choose a Attribute or select "none".'],
+        enum:{
+            values: ['none','fire', 'water', 'wind', 'earth', 'light', 'dark', 'poison', 'ice', 'electricity', 'ichor'],
+            message: '{VALUE} is not supported'
+        },
+        default: "none",
+    },
+    Material: {
+        type: String,
+        required: [true, 'must choose a Material'],
+        enum:{
+            values: ['wood', 'stone', 'leather', 'iron', 'bone', 'tungsten', 'steel', 'titanium', 'Meteorite', 'crimtane', 'mythril', 'orichalcum', 'adamantite', 'Netherite'],
             message: '{VALUE} is not supported'
         }
     },
+    Description: {
+        type: String,
+    },
+    Price:{
+        type: Number,
+        required: [true, 'Must set Price'],
+        minimum: 1
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now() 
+    },
+
 
 })
+module.exports = mongoose.model("Weapons", WeaponSchema)
