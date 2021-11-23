f8dc7dc40a49ce5a9ebf1900deef4c9e93d14c29
WRGcode <blueyeboyreid@gmail.com>
2021-11-23 10:22:40 -0700

    a little homework

--- Routes/Weapon.js
+++ Routes/Weapon.js
@@ -3,20 +3,14 @@ const router = express.Router();
 
 const {
     getAllEquipment,
-    getWeaponByName,
-    getWeaponByType,
-    getWeaponByAttributes,
-    getWeaponByMaterial,
-    getWeaponByPrice,
-    getInCart,
     createWeapon,
     EditWeapon,
     DeleteWeapon,
 } = require('../Controllers/Weapons')
 
-router.route("/").get(getAllEquipment).post(createWeapon)
+router.route("/").get(getAllEquipment).post(createWeapon).get(getWeaponByType).get(getWeaponByName).get(getWeaponByAttributes).get(getWeaponByMaterial).get(getWeaponByPrice).get(getInCart)
 router.route("/:id").put(EditWeapon).delete(DeleteWeapon)
-router.route("/by").get(getWeaponByType)(getWeaponByName).get(getWeaponByAttributes).get(getWeaponByMaterial).get(getWeaponByPrice).get(getInCart)
+
 
 
 
